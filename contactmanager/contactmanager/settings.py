960d62f29b4d6cd2936a0a692b3a279d36928b14
dduranvz <39139538+dduranvz@users.noreply.github.com>
2018-05-19 21:29:37 -0400

    Update settings.py

--- contactmanager/contactmanager/settings.py
+++ contactmanager/contactmanager/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'contactmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': database.sql,
     }
 }
 
