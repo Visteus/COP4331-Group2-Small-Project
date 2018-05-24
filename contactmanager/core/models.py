c124cc6976f2d08ae4281a5f18553500eb4be4d6
dduranvz <dduranvz@gmail.com>
2018-05-23 20:03:22 -0400

    Made a few changes to core directory. Added tables to models.py Added a simple message on the views folder for functionality in the mean time.

--- contactmanager/core/models.py
+++ contactmanager/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create users table
+class users(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
+ 
+# Create contacts table. "user_fk" is a foreign key to users
+# on_delete=CASCADE ==> if user gets deleted, drop all contacts    
+class contacts(models.Model):
+    user_fk = models.ForeignKey(users, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone_number = models.CharField(max_length=10)
+    email = models.EmailField()
