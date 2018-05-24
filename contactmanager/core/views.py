c0de78f584359b5ecd50cbfa70123946f79ad9bf
Visteus <chaulk.jacob@gmail.com>
2018-05-24 17:22:56 -0400

    added 'delete_contact' function

--- contactmanager/core/views.py
+++ contactmanager/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import users
+from .models import users, contacts
 from django.template.context_processors import request
 
 # Handle login request
@@ -11,3 +11,11 @@ def login(request):
 # Handle core page (Homepage?)
 def index(request):
     return HttpResponse("<h1>Homepage maybe? content in core/views.py<h1>")
+
+# Handle delete contact
+def delete_contact(request)
+	if request.method == 'POST':
+		cont_id = request.POST.get('contact_id')	# gets the requested contact's ID
+		user = users.objects.get(id = request.user)	# get the user object for the current user
+		user.contacts.filter(id = cont_id).delete()	# delete the requested contact from the user's list
+	return
