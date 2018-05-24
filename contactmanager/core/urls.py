8b5af19ec2c7bae291bdf1647b9a9639c8dd0995
Tuan Pham <tuanvpham32@outlook.com>
2018-05-24 19:27:07 -0400

    update url for contacts, login, logout

--- contactmanager/core/urls.py
+++ contactmanager/core/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.contrib.auth.views import logout
 from . import views
+
+app_name = 'core'
 
 urlpatterns = [
     # Core
-    path('', views.index, name='index'),
-    
-    # login
-    path('login', views.login, name='login'),
-]
+    path('contacts/', views.index, name='index'),
+    # login, logout
+    path('', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
+
+]   
