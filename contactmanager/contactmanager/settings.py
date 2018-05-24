e12eb73f6d7fc72d1346210ee5f58e8a86f3b8b0
Tuan Pham <tuanvpham32@outlook.com>
2018-05-24 19:24:59 -0400

    update setting with core app

--- contactmanager/contactmanager/settings.py
+++ contactmanager/contactmanager/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core.apps.CoreConfig',
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
