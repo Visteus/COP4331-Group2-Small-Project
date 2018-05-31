15de038ca57b1be38a20981f84c7b2fd164ec977
Harrison <Harrison.W.Black@gmail.com>
2018-05-30 22:53:49 -0400

    Added cool buttons for my functions

--- contactmanager/core/urls.py
+++ contactmanager/core/urls.py
@@ -6,9 +6,13 @@ app_name = 'core'
 
 urlpatterns = [
     # Core
-    path('contacts/', views.index, name='index'),
+    path('contacts/', views.contact_view, name='contact_view'),
     # login, logout
     path('', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
+    path('contacts/newcontact/', views.create_contact, name='create_contact'),
+    path('newuser/', views.create_user, name='create_user'),
+    path('', views.login_view, name='login_view'),
 
-]   
+
+]
--- contactmanager/core/views.py
+++ contactmanager/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.template import loader
@@ -34,7 +34,7 @@ def logout_view(request):
 def index(request):
 	return render(request, 'core/contact.html')
 
-@login_required(login_url='')
+#@login_required(login_url='')
 def create_user(request):
 	if request.method == 'POST':
 		username = request.POST['username']
@@ -62,7 +62,7 @@ def create_contact(request):
 		last_name = request.POST['lastname']
 		email = request.POST['email']
 		phone_number = request.POST['phonenumber']
-		new_contact = Contact.objects.create_contact(
+		new_contact = Contact.objects.create(
 			user = request.user,
 			first_name = first_name,
 			last_name = last_name,
@@ -72,3 +72,15 @@ def create_contact(request):
 		new_contact.save()
 		return HttpResponseRedirect(reverse('core:contact_view'))
 	return render(request, 'core/newcontact.html')
+
+@login_required(login_url='')
+def contact_view(request):
+	user_id = request.user.id
+	contact_list = Contact.objects.filter(user_id = user_id)
+	return render(
+		request,
+		'core/contact.html',
+		{
+			'contact_list': contact_list
+		}
+)
